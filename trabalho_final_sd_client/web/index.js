944d39142d6b2566b2b6b8814a38f608e203e28a
Gabriel Bernardi <Gabriel Bernardi@DESKTOP-FLEF9V5>
2017-06-24 21:12:47 -0300

    Testes

--- trabalho_final_sd_client/web/index.js
+++ trabalho_final_sd_client/web/index.js
@@ -1,23 +1,92 @@
-//checks if the browser supports WebRTC 
-
-function hasUserMedia() { 
-   navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia 
-      || navigator.mozGetUserMedia || navigator.msGetUserMedia; 
-   return !!navigator.getUserMedia; 
-}
+var connection = new WebSocket('ws://192.168.0.3:9095'); 
+var name = ""; 
  
-if (hasUserMedia()) { 
-   navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia
-      || navigator.mozGetUserMedia || navigator.msGetUserMedia;
-		
-   //get both video and audio streams from user's camera 
-   navigator.getUserMedia({ video: true, audio: true }, function (stream) { 
-      var video = document.querySelector('video'); 
-		
-      //insert stream into the video tag 
-      video.src = window.URL.createObjectURL(stream); 
-   }, function (err) {}); 
+var loginInput = document.querySelector('#loginInput'); 
+var loginBtn = document.querySelector('#loginBtn'); 
+var otherUsernameInput = document.querySelector('#otherUsernameInput'); 
+var connectToOtherUsernameBtn = document.querySelector('#connectToOtherUsernameBtn'); 
+var connectedUser, myConnection;
+  
+//when a user clicks the login button 
+loginBtn.addEventListener("click", function(event){ 
+   name = loginInput.value; 
 	
-} else {
-   alert("Error. WebRTC is not supported!"); 
-}
+   if(name.length > 0){ 
+      send({ 
+         type: "login", 
+         name: name 
+      }); 
+   } 
+	
+});
+  
+//handle messages from the server 
+connection.onmessage = function (message) { 
+   console.log("Got message", message.data);
+   var data = JSON.parse(message.data); 
+	
+   switch(data.type) { 
+      case "login": 
+         onLogin(data.success); 
+         break; 
+      case "offer": 
+         onOffer(data.offer, data.name); 
+         break; 
+      case "answer": 
+         onAnswer(data.answer); 
+         break; 
+      case "candidate": 
+         onCandidate(data.candidate); 
+         break; 
+      default: 
+         break; 
+   } 
+};
+  
+//when a user logs in 
+function onLogin(success) { 
+
+   if (success === false) { 
+      alert("oops...try a different username"); 
+   } else { 
+      //creating our RTCPeerConnection object 
+		
+      var configuration = { 
+         "iceServers": [{ "url": "stun:stun.1.google.com:19302" }] 
+      }; 
+		
+      myConnection = new webkitRTCPeerConnection(configuration); 
+      console.log("RTCPeerConnection object was created"); 
+      console.log(myConnection); 
+  
+      //setup ice handling
+      //when the browser finds an ice candidate we send it to another peer 
+      myConnection.onicecandidate = function (event) { 
+		
+         if (event.candidate) { 
+            send({ 
+               type: "candidate", 
+               candidate: event.candidate 
+            }); 
+         } 
+      }; 
+   } 
+};
+  
+connection.onopen = function () { 
+   console.log("Connected"); 
+};
+  
+connection.onerror = function (err) { 
+   console.log("Got error", err); 
+};
+  
+// Alias for sending messages in JSON format 
+function send(message) { 
+
+   if (connectedUser) { 
+      message.name = connectedUser; 
+   } 
+	
+   connection.send(JSON.stringify(message)); 
+};
